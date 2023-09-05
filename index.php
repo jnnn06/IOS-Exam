d9c0100f7f3ef19b569e2676ec67e87dfc48287c
junnnbit <junainamanangan.yns@gmail.com>
2023-09-05 14:36:05 +0800

    Created home page.

--- index.php
+++ index.php
@@ -1,39 +1,19 @@
-<?php
-require_once("database.php");
-
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["task"])) {
-    $task = $_POST["task"];
-    $sql = "INSERT INTO clients (name) VALUES ('test')";
-    $stmt = $mysqli->prepare($sql);
-    $stmt->bind_param("s", $task);
-    $stmt->execute();
-    $stmt->close();
-}
-
-$sql = "SELECT * FROM clients";
-$result = $mysqli->query($sql);
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple To-Do List</title>
+    <title>Tasks Pages</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <h1>Simple To-Do List</h1>
-    
-    <form method="POST">
-        <input type="text" name="task" placeholder="Add a task" required>
-        <button type="submit">Add</button>
-    </form>
+    <h1>Tasks Pages</h1>
 
     <ul>
-        <?php while ($row = $result->fetch_assoc()) : ?>
-            <li><?= $row["name"] ?></li>
-        <?php endwhile; ?>
+        <li><a href="/IOS-Exam/client-project.php">Task 1 - List of Clients' Projects</a></li>
+        <li><a href="/IOS-Exam/unpurchased-project.php">Task 2 - List of Unpurchased Projects</a></li>
+        <li><a href="/IOS-Exam//project-count.php">Task 3 - List Projects Count</a></li>
+        <li><a href="/IOS-Exam//jquery-color-change.html">Task 4 - JQuery Color Change On Click</a></li>
     </ul>
 </body>
 </html>
